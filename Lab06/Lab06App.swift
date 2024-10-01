6e40111f8e991fb8dcd8fdec0a3523ff89dabe41
joelward2025 <joelward@cedarville.edu>
2024-10-01 10:40:03 -0400

    Initial Commit

---
+++ Lab06/Lab06App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab06App.swift
+//  Lab06
+//
+//  Created by Joel Ward on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab06App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
